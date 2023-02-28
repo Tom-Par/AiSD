9f30d1e42c9fa899e0e8a3d812c6a471a431a870
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-02-28 20:17:23 +0100

    zad 6 update

---
+++ Lista_1/zad_6.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+struct lnode {
+    int key;
+    lnode* next;
+    lnode(int k, lnode* n=nullptr):key(k), next(n){}
+};
+
+// a
+void write_out(lnode* L) {
+
+};
+
+// b
+void sum(lnode* L) {
+    int result = 0;
+    while(L!=nullptr)
+        result += L->key;
+        L=L->next;
+};
+
+//c
+void nth_element(int n, lnode *L) {
+
+};
+
+// d
+void insert(lnode* L, int x) {
+    L = new lnode(x, L);
+};
+
+//e
+void insert_after_smaller(lnode *&L, int x) {
+
+};
+
+//f
+void remove(lnode* &L, int x) {
+    lnode * t=L;
+    L = t->next;
+    delete t;
+};
+
+//g
+void filter(lnode* &L, bool(*cond)(int)) {
+
+};
+
+//h
+void destroy(lnode* &L) {
+
+};
+
+int main() {
+    lnode* L=NULL;
+
+    for(int i=0; i<10; i++)
+        insert(L,i*12);
+
+    while (L)
+    {
+        cout << L->key << endl;
+        remove(L)
+    }
+    
+}
