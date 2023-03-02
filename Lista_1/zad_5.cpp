0d906aeccb6341c6a61e9747f24e1f41b1f75b62
Tomasz Paruzel <tom.krzy.par@gmail.com>
2023-03-02 10:45:26 +0100

    task 5 add on

--- Lista_1/zad_5.cpp
+++ Lista_1/zad_5.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
+int counter = 0;
 double oblicz(vector<double> a, double x) {
     double result = 0.0;
     int power = a.size()-1;
-    for(int i = power; i >= 0; i--) {
+    for(int i = power; i > 0; i--) {
         result =a[i] + result*x;
+        counter++;
     }
     return result;
 }
 
 int main() {
     cout << oblicz({4,5,3,0,0,18}, 10) << endl;
+    cout << counter << " mnozen"<<endl;
     return 0;
 }
